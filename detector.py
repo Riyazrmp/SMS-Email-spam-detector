6a7ec9b21ba86cc213676643f7cf71ae0c3050aa
Rehman Riyaz Ur <riyaz@connect.hku.hk>
2024-10-25 03:41:22 +0800

    Update detector.py

--- detector.py
+++ detector.py
@@ -5,7 +5,7 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string
 nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+nltk.download('all')
 nltk.download('stopwords')
 
 port = PorterStemmer()
